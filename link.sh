0a8790edf35c4b4115f8bb206aee319e65142986
Charles Lavery <charles.lavery@gmail.com>
2012-12-05 19:11:16 -0500

    link script

---
+++ link.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+HOSTNAME=`hostname -s`
+HOST_OS=`uname|tr '[:upper:]' '[:lower:]'`
+
+find .* -maxdepth 0 -not \( -name '.' -or -name '..' \) -print -exec ln -fs $(pwd)/{} $HOME \;
+
+case $HOST_OS in
+  darwin)
+    rm -f $HOME/.tmux-linux.conf
+  ;;
+  linux)
+    rm -f $HOME/.tmux-osx.conf
+    rm -f $HOME/.osx
+  ;;
+esac
