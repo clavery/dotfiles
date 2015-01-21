2f520f27aed86658a07c4b571ce62d6892eb93f6
Charles Lavery <clavery@pixelmedia.com>
2015-01-21 13:50:55 -0500

    pomo script updates

--- bin/pomo.py
+++ bin/pomo.py
@@ -25,18 +25,39 @@ quit_parser = subparsers.add_parser('quit', help='Quit background process')
 def say(message):
     os.system('osascript -e \'say "%s" using "Vicki"\'' % message)
 
+def notify(title, message):
+    os.system('osascript -e \'display notification "{0}" with title "{1}"\''.format(message, title))
+
+
+DEFAULT = "#[default]"
+GREEN = "#[default]#[fg=green]"
+YELLOW = "#[default]#[fg=yellow]"
+RED = "#[default]#[fg=red]"
+
 
 class Pomo(object):
-    def __init__(self, desc, length=25):
+    def __init__(self, desc, tmuxfile, length=25):
         self.desc = desc
         self.length = length
         self.timer = None
+        self.tmuxfile = tmuxfile
 
     def report_remaining(self, remaining):
         say("{0} minutes".format(remaining))
 
     def report_done(self):
         say("{0} -- is finished".format(self.desc))
+        notify("Pomo", "{0} -- FINISHED".format(self.desc))
+
+    def write_tmux(self, remaining):
+        if remaining > 10:
+            val = "{0}{1}:00{2}".format(GREEN, remaining, DEFAULT)
+        elif remaining > 5:
+            val = "{0}{1}:00{2}".format(YELLOW, remaining, DEFAULT)
+        else:
+            val = "{0}{1}:00{2}".format(RED, remaining, DEFAULT)
+        with open(self.tmuxfile, 'w') as f:
+            f.write(val)
 
     def do_interval(self, interval):
         minute = self.length - interval
@@ -44,15 +65,24 @@ class Pomo(object):
         if minute in [24, 10, 5, 1]:
             self.report_remaining(minute)
 
-        timer = Timer(60, self.do_interval, args=[interval + 1])
-        timer.start()
+        self.write_tmux(minute)
+
+        if minute > 0:
+            self.timer = Timer(60, self.do_interval, args=[interval + 1])
+            self.timer.start()
+        else:
+            self.report_done()
 
     def start(self):
         timer = Timer(60, self.do_interval, args=[1])
         timer.start()
         self.timer = timer
+        self.write_tmux(self.length)
 
     def cancel(self):
+        with open(self.tmuxfile, 'w') as f:
+            f.write("N/A")
+
         if self.timer:
             self.timer.cancel()
 
@@ -68,7 +98,7 @@ class PomoCli(Daemon):
         if desc is None:
             desc = "N/A"
 
-        pomo = Pomo(desc, 25)
+        pomo = Pomo(desc, self.tmuxfile, 25)
         pomo.start()
         self.pomo = pomo
 
